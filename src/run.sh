b70c9a2512c99b5723ded879ec5b35f8353ed1f3
ziaoang <ziaoang@gmail.com>
2016-07-14 16:35:32 +0800

    add run list

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,15 @@
 date
-python char_rnn.py
+python simple_char_cnn_1.py
 date
-python char_cnn.py
+python simple_char_cnn_2.py
+date
+python simple_char_cnn_3.py
+date
+python simple_char_cnn_4.py
+date
+python simple_char_cnn_5.py
+date
+python simple_char_cnn_6.py
+date
+python simple_char_cnn_7.py
 date
